46cdbb3748a586ae92bee74b6b2fe68f55b54ea5
Denis Nikitin <150063921+n1kk113@users.noreply.github.com>
2024-01-22 01:43:23 +0300

    Update NewHomeworkFive.java

--- src/test/java/NewHomeworkFive.java
+++ src/test/java/NewHomeworkFive.java
@@ -1,6 +1,5 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.DragAndDropOptions;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
